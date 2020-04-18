a83a5e71246e8547854f70b97194d5b4fed44a4a
Proenix <piotr26ch@gmail.com>
2020-04-18 20:32:01 +0200

    Reading from internal/external app files.

---
+++ lab4readaloud/app/src/main/java/pl/proenix/android/us2pum/lab4readaloud/FileChooser.java
@@ -0,0 +1,117 @@
+package pl.proenix.android.us2pum.lab4readaloud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.File;
+
+public class FileChooser extends AppCompatActivity {
+
+    private LinearLayout llFileChooser;
+    private TextView textViewCurentPath;
+    private File currentDir;
+    private File[] currentDirFiles;
+    private String lastDirOpenPath;
+
+    private Button back;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_chooser);
+
+        llFileChooser = findViewById(R.id.llFileChooser);
+        textViewCurentPath = findViewById(R.id.textViewCurrentPath);
+
+        // Get lastDirOpenPath from MainActivity
+        Intent intent = getIntent();
+        lastDirOpenPath = intent.getStringExtra("lastDirOpenPath");
+
+        // Load file list view.
+        refreshView(lastDirOpenPath);
+    }
+
+    /**
+     * Prepare items on scrollView.
+     * @param selectedDir Absolute path to directory that is to be displayed.
+     */
+    private void refreshView(final String selectedDir) {
+        try {
+            llFileChooser.removeAllViews();
+            textViewCurentPath.setText(lastDirOpenPath);
+
+            currentDir = new File(selectedDir);
+            currentDirFiles = currentDir.listFiles();
+            File file;
+
+            back = new Button(this);
+            back.setText("..");
+            back.setId(View.generateViewId());
+            llFileChooser.addView(back);
+            back.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    Toast.makeText(FileChooser.this, "..", Toast.LENGTH_SHORT).show();
+                    try {
+                        String newPath = currentDir.getParentFile().getAbsolutePath();
+                        lastDirOpenPath = newPath;
+                    } catch (NullPointerException e) {
+                        Toast.makeText(FileChooser.this, "Parent does not exist.", Toast.LENGTH_SHORT).show();
+                        e.printStackTrace();
+                    }
+                    refreshView(lastDirOpenPath);
+                }
+            });
+            for (int i = 1; i < currentDirFiles.length; i++) {
+                file = currentDirFiles[i];
+
+                Button et = new Button(this);
+                et.setId(i);
+                if (file.isDirectory()) {
+                    et.setText("[DIR] " + currentDirFiles[i].getName());
+                } else {
+                    et.setText(file.getName());
+                }
+                et.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        String newPath = currentDirFiles[v.getId()].getAbsolutePath();
+                        // Check if selected Path is Directory
+                        File tmp = new File(newPath);
+                        if (tmp.isDirectory()) {
+                            lastDirOpenPath = newPath;
+                            refreshView(lastDirOpenPath);
+                        } else {
+                            Intent intent = new Intent();
+                            intent.putExtra("lastDirOpenPath", lastDirOpenPath);
+                            intent.putExtra("selectedDir", newPath);
+                            setResult(RESULT_OK, intent);
+                            FileChooser.super.onBackPressed();
+                        }
+                    }
+                });
+                llFileChooser.addView(et);
+            }
+        } catch (Exception e) {
+            Log.e("FileManipulation", lastDirOpenPath +"  "+ e.toString());
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent();
+        intent.putExtra("lastDirOpenPath", lastDirOpenPath);
+        setResult(RESULT_OK, intent);
+        super.onBackPressed();
+    }
+}
--- lab4readaloud/app/src/main/java/pl/proenix/android/us2pum/lab4readaloud/MainActivity.java
+++ lab4readaloud/app/src/main/java/pl/proenix/android/us2pum/lab4readaloud/MainActivity.java
@@ -1,21 +1,44 @@
 package pl.proenix.android.us2pum.lab4readaloud;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Environment;
 import android.speech.tts.TextToSpeech;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.SeekBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.SimpleFileVisitor;
 import java.util.Locale;
 
 public class MainActivity extends AppCompatActivity implements TextToSpeech.OnInitListener, View.OnClickListener {
 
+    private final static int REQUEST_CODE_CHOOSE_FILE = 1;
+
     private TextToSpeech tts;
-    private Button buttonSpeakEn, buttonSpeakPl;
+    private float readingSpeed = 1;
+    private Button buttonSpeakEn, buttonSpeakPl, buttonOpenFile;
     private EditText editTextSpeak;
+    private SeekBar seekBarSpeakSpeed;
+    private TextView textViewReadSpeed;
+
+    private String lastDirOpenPath;
+    private String fileToRead;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -28,11 +51,67 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         buttonSpeakEn = findViewById(R.id.buttonSpeakEn);
         buttonSpeakPl = findViewById(R.id.buttonSpeakPl);
         editTextSpeak = findViewById(R.id.editTextSpeak);
+        seekBarSpeakSpeed = findViewById(R.id.seekBar);
+        textViewReadSpeed = findViewById(R.id.textViewReadSpeed);
+        textViewReadSpeed.setText(String.valueOf(readingSpeed));
+        buttonOpenFile = findViewById(R.id.buttonOpenFile);
 
         buttonSpeakEn.setOnClickListener(this);
         buttonSpeakPl.setOnClickListener(this);
+        buttonOpenFile.setOnClickListener(this);
 
-        //editTextSpeak.setText("Litwo! Ojczyzno moja! Ty jesteś jak zdrowie. Ile cię stracił. Dziś piękność twą w głównym sądzie w tkackim pudermanie). Wdział więc, jak roratne świéce. Pierwsza z krzykiem podróżnego barwą spłonęła rumian jak drudzy i psy za nim");
+        if (Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED ||
+            Environment.getExternalStorageState() == Environment.MEDIA_MOUNTED_READ_ONLY) {
+            lastDirOpenPath = Environment.getRootDirectory().getAbsolutePath();
+        } else {
+            lastDirOpenPath = getFilesDir().getAbsolutePath();
+        }
+
+        // Create sample file
+        String filename = "sample.text";
+        String fileContents = "Litwo! Ojczyzno moja! Ty jesteś jak zdrowie. Ile cię stracił. Dziś piękność twą w głównym sądzie w tkackim pudermanie). Wdział więc, jak roratne świéce. Pierwsza z krzykiem podróżnego barwą spłonęła rumian jak drudzy i psy za nim";
+        Context context = getBaseContext();
+        try (FileOutputStream fos = context.openFileOutput(filename, Context.MODE_PRIVATE)) {
+            fos.write(fileContents.getBytes());
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+
+        // Sets speed of reading. Check seekBar bar progress.
+        seekBarSpeakSpeed.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                switch (progress) {
+                    case 0:
+                        readingSpeed = (float) 0.5;
+                        break;
+                    case 1:
+                        readingSpeed = (float) 0.75;
+                        break;
+                    case 3:
+                        readingSpeed = (float) 1.5;
+                        break;
+                    case 4:
+                        readingSpeed = (float) 2.0;
+                        break;
+                    case 2:
+                    default:
+                        readingSpeed = (float) 1.0;
+                }
+                textViewReadSpeed.setText(String.valueOf(readingSpeed));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {}
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) { }
+        });
+
+        editTextSpeak.setText("Litwo! Ojczyzno moja! Ty jesteś jak zdrowie. Ile cię stracił. Dziś piękność twą w głównym sądzie w tkackim pudermanie). Wdział więc, jak roratne świéce. Pierwsza z krzykiem podróżnego barwą spłonęła rumian jak drudzy i psy za nim");
     }
 
     @Override
@@ -44,6 +123,10 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         super.onDestroy();
     }
 
+    /**
+     * Check if Text To Speech module is loaded correctly.
+     * @param status Status of Text To Speech initialization.
+     */
     @Override
     public void onInit(int status) {
         if (status == TextToSpeech.SUCCESS) {
@@ -54,12 +137,17 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         }
     }
 
-
+    /**
+     * Run action of reading aloud of provided text.
+     * @param language String representation of Locale object.
+     * @param text Text to be read aloud.
+     */
     private void speakAloud(String language, String text) {
         int result = tts.setLanguage(localeFromString(language));
         if (result == TextToSpeech.LANG_MISSING_DATA || result == TextToSpeech.LANG_NOT_SUPPORTED) {
             Toast.makeText(this, "Text To Speech language not supported or package missing.", Toast.LENGTH_LONG).show();
         } else {
+            tts.setSpeechRate(readingSpeed);
             tts.speak(text, TextToSpeech.QUEUE_FLUSH, null, "1");
         }
     }
@@ -87,6 +175,10 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         }
     }
 
+    /**
+     * On click handler for Main Activity.
+     * @param v View of Main Activity.
+     */
     @Override
     public void onClick(View v) {
         if (v.getId() == buttonSpeakEn.getId()) {
@@ -94,6 +186,42 @@ public class MainActivity extends AppCompatActivity implements TextToSpeech.OnIn
         }
         if (v.getId() == buttonSpeakPl.getId()) {
             speakAloud("pl_PL", editTextSpeak.getText().toString());
+        }
+        if (v.getId() == buttonOpenFile.getId()) {
+            Intent intent = new Intent(getApplicationContext(), FileChooser.class);
+            intent.putExtra("lastDirOpenPath", lastDirOpenPath);
+            startActivityForResult(intent, REQUEST_CODE_CHOOSE_FILE);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        switch (requestCode) {
+            case REQUEST_CODE_CHOOSE_FILE:
+                if (resultCode == RESULT_OK) {
+                    lastDirOpenPath = data.getStringExtra("lastDirOpenPath");
+                    fileToRead = data.getStringExtra("selectedDir");
+
+                    File file = new File(fileToRead);
+                    StringBuilder text = new StringBuilder();
+                    try {
+                        BufferedReader br = new BufferedReader(new FileReader(file));
+                        String line;
+
+                        while ((line = br.readLine()) != null) {
+                            text.append(line);
+                            text.append('\n');
+                        }
+                        br.close();
+                    } catch (FileNotFoundException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                    editTextSpeak.setText(text.toString());
+                }
+                break;
         }
     }
 }
