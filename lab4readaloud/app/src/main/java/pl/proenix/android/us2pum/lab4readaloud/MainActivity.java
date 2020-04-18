74a2a31a3ec7a1bfd8ed647756c86094da92416a
Proenix <piotr26ch@gmail.com>
2020-04-18 12:22:48 +0200

    Read Aloud init.

---
+++ lab4readaloud/app/src/main/java/pl/proenix/android/us2pum/lab4readaloud/MainActivity.java
@@ -0,0 +1,14 @@
+package pl.proenix.android.us2pum.lab4readaloud;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
