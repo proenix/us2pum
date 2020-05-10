9d502e5c9e716557d226f12b42ec91dc666301e7
Proenix <piotr26ch@gmail.com>
2020-05-10 14:14:04 +0200

    Layout for displaying notes in List.

--- lab6notes/app/src/main/java/pl/proenix/android/us2pum/lab6notes/NotesListFragment.java
+++ lab6notes/app/src/main/java/pl/proenix/android/us2pum/lab6notes/NotesListFragment.java
@@ -1,13 +1,16 @@
 package pl.proenix.android.us2pum.lab6notes;
 
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffColorFilter;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.fragment.NavHostFragment;
 
 /**
  * Fragment for displaying list of all notes.
@@ -35,13 +38,34 @@ public class NotesListFragment extends Fragment {
 
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
-        
-        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                NavHostFragment.findNavController(NotesListFragment.this)
-                        .navigate(R.id.action_notesListFragment_to_noteCreateUpdateFragment);
-            }
-        });
+
+        // TODO: 10/05/2020 Implement popup on sort filter clicks.
+        LinearLayout linearLayoutNotesList = view.findViewById(R.id.linearLayoutNotesList);
+
+        for (int i = 0; i < 10; i++) {
+            View singleNoteRow = View.inflate(view.getContext(), R.layout.fragment_note_row, null);
+
+            // Get background shape and color it depending on category
+            // TODO: 10/05/2020 Color on category.
+            try {
+                Drawable bg = getContext().getDrawable(R.drawable.layout_note_row_bg);
+                bg.setColorFilter(new PorterDuffColorFilter(getResources().getColor(R.color.colorPrimary), PorterDuff.Mode.SRC));
+
+                // TODO: 10/05/2020 Add on click.
+
+                singleNoteRow.findViewById(R.id.noteElement).setBackground(bg);
+            } catch (NullPointerException ignored) { }
+
+            linearLayoutNotesList.addView(singleNoteRow);
+        }
+
+
+//        view.findViewById(R.id.button_first).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                NavHostFragment.findNavController(NotesListFragment.this)
+//                        .navigate(R.id.action_notesListFragment_to_noteCreateUpdateFragment);
+//            }
+//        });
     }
 }
